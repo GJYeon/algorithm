31c2ac8726ea9ea3fa9206ec8847931c3e422e45
조준연 <132131162+GJYeon@users.noreply.github.com>
2026-09-11 17:47:45 +0900

    [level 3] Title: 네트워크, Time: 0.46 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,43 @@
 import java.util.*;
 class Solution {
-    Queue<Integer> queue = new LinkedList<>();
-    
-    public void bfs(int start, boolean[] visited, int[][] computers) {
-        visited[start] = true;
-        
-        for (int i = 0 ; i < computers.length ; i++) {
-            if (computers[start][i] == 1 && visited[i]==false) {
-                visited[i] = true;
-                queue.add(i);
-            }
-        }
-        
-        while (!queue.isEmpty()) {
-            int s = queue.poll();
-            bfs(s, visited, computers);
-        }
-    }
-    
+    Queue<Node> queue = new LinkedList<>();
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[computers.length];
-        for (int i = 0 ; i < computers.length ; i++) {
-            if (visited[i]==false) {
+        boolean[] visited = new boolean[n];
+        for (int i = 0 ; i < n ; i++) {
+            if (computers[i][i] == 1 && visited[i] == false) {
+                queue.add(new Node(i));
+                visited[i] = true;
                 answer += 1;
-                bfs(i, visited, computers);
+            } 
+            
+            while(!queue.isEmpty()) {
+                Node node = queue.poll();
+
+                bfs(visited, node, computers, n);
             }
         }
         
+        
+        
         return answer;
+    }
+    
+    public void bfs(boolean[] visited, Node node, int[][] computers, int n) {
+        for (int j = 0 ; j < n ; j++) {
+            if (computers[node.node][j]==1 && visited[j] == false) {
+                queue.add(new Node(j));
+                visited[j] = true;
+            }
+        }
+        
+    }
+}
+
+class Node {
+    int node;
+    
+    public Node(int node) {
+        this.node = node;
     }
 }
